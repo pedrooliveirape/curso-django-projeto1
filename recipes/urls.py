0647346f61fd4661010a12bad116f735a69e5665
pedrooliveirape <pedro.oliveirape@hotmail.com>
2022-04-07 11:05:04 -0300

    removed contato and sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 
-from recipes.views import home, sobre, contato
+from recipes.views import home
 
 urlpatterns = [
     path('', home),
     path('home/', home),
-    path('contato/', contato),
-    path('sobre/', sobre),
 ]
