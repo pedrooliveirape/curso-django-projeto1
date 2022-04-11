2135753e6d887c76979198fe9539f8b3c5cafd42
pedrooliveirape <pedro.oliveirape@hotmail.com>
2022-04-11 13:04:54 -0300

    new view URL and new recipe template

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from recipes.views import home
+from recipes import views
 
 urlpatterns = [
-    path('', home),
-    path('home/', home),
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipes),
 ]
